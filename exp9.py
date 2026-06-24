bdd29839396afa12e77d4711a8c227b7c6674f5f
daab2 <xaura545@gmail.com>
2026-06-24 20:58:48 +0530

    Update exp9.py

--- exp9.py
+++ exp9.py
@@ -1,70 +1,41 @@
-import spidev
-import time
 import RPi.GPIO as GPIO
 import requests
+import time
 
-# -------------------------------
-# GPIO SETUP
-# -------------------------------
+LDR_PIN = 17
 LED_PIN = 18
+
 GPIO.setmode(GPIO.BCM)
+GPIO.setup(LDR_PIN, GPIO.IN)
 GPIO.setup(LED_PIN, GPIO.OUT)
 
-# -------------------------------
-# SPI (MCP3008) SETUP
-# -------------------------------
-spi = spidev.SpiDev()
-spi.open(0, 0)   # Bus 0, Device 0
-spi.max_speed_hz = 1350000
-
-def read_adc(channel):
-    adc = spi.xfer2([1, (8 + channel) << 4, 0])
-    data = ((adc[1] & 3) << 8) + adc[2]
-    return data
-
-# -------------------------------
-# CLOUD (ThingSpeak)
-# -------------------------------
-API_KEY = "YOUR_API_KEY"
+API_KEY = "your_write_api_key"
 URL = "https://api.thingspeak.com/update"
 
-# -------------------------------
-# THRESHOLD
-# -------------------------------
-THRESHOLD = 400   # Adjust after testing
-
-# -------------------------------
-# MAIN LOOP
-# -------------------------------
 try:
-    while True:
-        light_level = read_adc(0)   # Channel 0
-
-        # Light control
-        if light_level < THRESHOLD:
-            GPIO.output(LED_PIN, GPIO.HIGH)
-            status = 1   # Light ON
-        else:
-            GPIO.output(LED_PIN, GPIO.LOW)
-            status = 0   # Light OFF
-
-        print(f"LDR Value: {light_level} | LED: {status}")
-
-        # Send to cloud
-        payload = {
-            "api_key": API_KEY,
-            "field1": light_level,
-            "field2": status
-        }
-
-        try:
-            requests.get(URL, params=payload)
-        except:
-            print("Cloud update failed")
-
-        time.sleep(15)   # ThingSpeak limit
-
+	while True:
+		ldr_value = GPIO.input(LDR_PIN)
+		if ldr_value == 0:
+			GPIO.output(LED_PIN, GPIO.HIGH)
+			light_status = "ON"
+			print("Dark Detected\nLED ON")
+		else:
+			GPIO.output(LED_PIN, GPIO.LOW)
+			light_status = "OFF"
+			print("Bright Detected\nLED OFF")
+		payload = {
+			"api_key": API_KEY,
+			"field1" : ldr_value,
+			"field2" : 1 if light_status == "ON" else 0
+		}
+		try:
+			response = requests.get(URL, params = payload)
+			if response.status_code == 200:
+				print("Data uploaded to ThingSpeak")
+			else:
+				print("Upload Failed")
+		except Exception as e:
+			print("Internet Error:", e)
 except KeyboardInterrupt:
-    GPIO.cleanup()
-    spi.close()
-
+	GPIO.cleanup()
+	print("Program Stopped")			
